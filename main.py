a30ca988d288f6bf50b59e117bb9846f997278d0
Ryan Parrish <rparrish@nxt.tech>
2024-11-06 18:26:55 -0500

    * fix logging * refactor some bits

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import datetime
 import os
+import logging
+from typing import List, Tuple
 
 import environs
 import flask
@@ -9,6 +11,8 @@ from flask import Response
 from flask_apscheduler import APScheduler
 from icalevents.icalevents import events
 from openai import OpenAI
+
+logging.basicConfig(level=logging.DEBUG)  # Set the desired logging level
 
 env = environs.Env()
 environs.Env.read_env()
@@ -39,7 +43,7 @@ ha_session = requests.Session()
 ha_session.headers.update({"Authorization": f"Bearer {HA_TOKEN}"})
 
 
-def get_ha_data():
+def get_ha_data() -> Tuple[dict, dict]:
     ha = ha_session.get(
         "http://homeassistant.local:8123/api/states/climate.main_floor",
     )
@@ -52,14 +56,16 @@ def get_ha_data():
     return main_floor, zone_home
 
 
-def fetch_calendar():
+def fetch_calendar() -> List[str]:
     today = datetime.date.today()
     # The `start`, and `end` arguments of events() do not work as expected, so we filter the results manually
     es = events(ICAL_URL, fix_apple=True, sort=True)
-    return [e.summary for e in es if e.start.date() == today]
+    cal_events = [e.summary for e in es if e.start.date() == today]
+    app.logger.info("cal_events: %s", cal_events)
+    return cal_events
 
 
-def build_prompt(main_floor, zone_home):
+def build_prompt(main_floor: dict, zone_home:dict):
     forecast = pirateweather.load_forecast(
         PIRATE_WEATHER_API_KEY,
         zone_home["attributes"]["latitude"],
@@ -76,20 +82,21 @@ def build_prompt(main_floor, zone_home):
         "High Temperature": f"{round(today.d['apparentTemperatureHigh'])}°F",
         "Low Temperature": f"{round(today.d['apparentTemperatureLow'])}°F",
     }
-
-    prompt = f"""Write a good morning message to Christa without using emojis or signing the message, given the following weather conditions for today:
+    cal_events = fetch_calendar()
+    prompt = f"""Write me a good morning message without using emojis or signing the message, given the following weather conditions for today:
                           Temperature inside the house: {forcast_prompt['Inside Temperature']}
                           Current Conditions: {forcast_prompt['Current Conditions']}
                           Current Temp: {forcast_prompt['Current Temperature']}
                           Conditions for the day: {forcast_prompt['Conditions for Day']}
                           High:  {forcast_prompt['High Temperature']}
-                          Low: {forcast_prompt['Low Temperature']}
-               And the following items on her calendar for today: {','.join(fetch_calendar())}"""
+                          Low: {forcast_prompt['Low Temperature']}"""
+    if cal_events:
+        prompt += f"\n\nAnd Today's calendar events: {', '.join(cal_events)}"
     app.logger.info(prompt)
     return prompt
 
 
-def build_response():
+def build_response() -> str:
     prompt = build_prompt(*get_ha_data())
 
     response = client.chat.completions.create(
@@ -112,10 +119,7 @@ def build_response():
     return response.choices[0].message.content
 
 
-@scheduler.task(
-    "cron", id="write_message", week="*", day_of_week="*", hour=HOUR, minute=MINUTE
-)
-def _write_message():
+def _write_message() -> str:
     message = build_response()
 
     with open(f"{dir_path}/message.txt", "w") as f:
@@ -124,15 +128,23 @@ def _write_message():
     return message
 
 
+@scheduler.task(
+    "cron", id="write_message", week="*", day_of_week="*", hour=HOUR, minute=MINUTE
+)
+def scheduled_message():
+    app.logger.info("Writing scheduled message")
+    _write_message()
+
+
 @app.route("/write_message")
-def write_message():
+def write_message() -> Response:
     message = _write_message()
 
     return Response(message, mimetype="text/plain")
 
 
 @app.route("/")
-def get_message():
+def get_message() -> Response:
     with open(f"{dir_path}/message.txt", "r") as f:
         return Response(f.read(), mimetype="text/plain")
 
