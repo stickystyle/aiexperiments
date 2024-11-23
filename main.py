178b1c68db948c9f5bfcfe8b511b4c3291ddb2ce
Ryan Parrish <rparrish@nxt.tech>
2024-11-22 20:20:59 -0500

    * add open weather because pirateweather weather gives bad data * add good news for the day * rather than build up data structures, build up prompts * Use HA API client

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import datetime
-import os
 import logging
 from typing import List, Tuple
 
 import environs
+import feedparser
 import flask
-import pirateweather
+import holidays
 import requests
 from flask import Response
+from homeassistant_api import Client
 from icalevents.icalevents import events
 from openai import OpenAI
-import holidays
 
 env = environs.Env()
 environs.Env.read_env()
@@ -23,7 +23,6 @@ app = flask.Flask(__name__)
 
 GITHUB_TOKEN = env("GITHUB_TOKEN")
 PIRATE_WEATHER_API_KEY = env("PIRATE_WEATHER_API_KEY")
-HA_TOKEN = env("HA_TOKEN")
 PERSONALITY = env("PERSONALITY")
 ICAL_URL = env("ICAL_URL")
 
@@ -32,35 +31,79 @@ client = OpenAI(
     api_key=GITHUB_TOKEN,
 )
 
-ha_session = requests.Session()
-ha_session.headers.update({"Authorization": f"Bearer {HA_TOKEN}"})
+
+ha_client = Client("http://homeassistant.local:8123/api", env("HA_TOKEN"))
 
 
-def get_ha_data() -> Tuple[dict, dict]:
-    ha = ha_session.get(
-        "http://homeassistant.local:8123/api/states/climate.main_floor",
+def _get_pirate_weather(latitude, longitude) -> str:
+    import pirateweather
+
+    forecast = pirateweather.load_forecast(
+        key=PIRATE_WEATHER_API_KEY,
+        lat=latitude,
+        lng=longitude,
     )
-    main_floor = ha.json()
-    app.logger.debug("main_floor: %s", main_floor)
 
-    ha = ha_session.get(
-        "http://homeassistant.local:8123/api/states/zone.home",
+    by_hour = forecast.hourly()
+    today = forecast.daily().data[0]
+
+    return f"""Current Conditions: {forecast.currently().summary}
+              Current Temp: {round(forecast.currently().apparentTemperature)}°F
+              Conditions for the day: {by_hour.summary}
+              High:  {round(today.d["apparentTemperatureHigh"])}°F
+              Low: {round(today.d["apparentTemperatureLow"])}°F"""
+
+
+def _get_open_weather(latitude, longitude) -> str:
+    res = requests.get(
+        "https://api.openweathermap.org/data/3.0/onecall",
+        params={
+            "lat": latitude,
+            "lon": longitude,
+            "exclude": "minutely,hourly",
+            "units": "imperial",
+            "appid": env("OPEN_WEATHER_API_KEY"),
+        },
     )
-    zone_home = ha.json()
+    res.raise_for_status()
+    data = res.json()
+    return f"""Current Conditions: {data['current']['weather'][0]['description']}
+          Current Temp: {round(data['current']['feels_like'])}°F
+          Conditions for the day: {data['daily'][0]['weather'][0]['description']}
+          Morning Temp: {round(data['daily'][0]['temp']['morn'])}°F
+          Day Temp: {round(data['daily'][0]['temp']['day'])}°F
+          Evening Temp: {round(data['daily'][0]['temp']['eve'])}°F
+          Nighttime Temp: {round(data['daily'][0]['temp']['night'])}°F"""
+
+
+def get_weather() -> str:
+    latitude, longitude = get_location_from_ha()
+    return _get_open_weather(latitude, longitude)
+
+
+def get_indoor_temperature() -> int:
+    with ha_client:
+        main_floor = ha_client.get_state(entity_id="climate.main_floor")
+    return main_floor.attributes["current_temperature"]
+
+
+def get_location_from_ha() -> Tuple[float, float]:
+    with ha_client:
+        zone_home = ha_client.get_state(entity_id="zone.home")
     app.logger.debug("zone_home: %s", zone_home)
-    return main_floor, zone_home
+    return zone_home.attributes["latitude"], zone_home.attributes["longitude"]
 
 
-def fetch_calendar() -> List[str]:
+def fetch_calendar() -> str:
     today = datetime.date.today()
-    us_holidays = holidays.country_holidays('US')
+    us_holidays = holidays.country_holidays("US")
     # The `start`, and `end` arguments of events() do not work as expected, so we filter the results manually
     es = events(ICAL_URL, fix_apple=True, sort=True)
     cal_events = [e.summary for e in es if e.start.date() == today]
     if today_holidays := us_holidays.get(today):
         cal_events.append(today_holidays)
     app.logger.info("cal_events: %s", cal_events)
-    return cal_events
+    return ", ".join(cal_events).strip()
 
 
 def get_time_of_day() -> str:
@@ -75,39 +118,56 @@ def get_time_of_day() -> str:
         return "Night"
 
 
-def build_prompt(main_floor: dict, zone_home: dict):
-    forecast = pirateweather.load_forecast(
-        PIRATE_WEATHER_API_KEY,
-        zone_home["attributes"]["latitude"],
-        zone_home["attributes"]["longitude"],
-    )
+def fetch_good_news():
+    feed = feedparser.parse("https://www.goodnewsnetwork.org/feed/")
+    return feed.entries[1].link
 
-    by_hour = forecast.hourly()
-    today = forecast.daily().data[0]
-    forcast_prompt = {
-        "Inside Temperature": f"{main_floor['attributes']['current_temperature']}°F",
-        "Current Conditions": forecast.currently().summary,
-        "Current Temperature": f"{round(forecast.currently().apparentTemperature)}°F",
-        "Conditions for Day": by_hour.summary,
-        "High Temperature": f"{round(today.d['apparentTemperatureHigh'])}°F",
-        "Low Temperature": f"{round(today.d['apparentTemperatureLow'])}°F",
-    }
-    cal_events = fetch_calendar()
-    prompt = f"""Write me a good {get_time_of_day()} message without using emojis or signing the message, given the following weather conditions for today:
-                          Temperature inside the house: {forcast_prompt['Inside Temperature']}
-                          Current Conditions: {forcast_prompt['Current Conditions']}
-                          Current Temp: {forcast_prompt['Current Temperature']}
-                          Conditions for the day: {forcast_prompt['Conditions for Day']}
-                          High:  {forcast_prompt['High Temperature']}
-                          Low: {forcast_prompt['Low Temperature']}"""
+
+def build_prompt():
+
+    try:
+        good_news = fetch_good_news()
+    except Exception as e:
+        app.logger.error("Failed to fetch good news: %s", e)
+        good_news = None
+
+    try:
+        inside_temperature = get_indoor_temperature()
+    except Exception as e:
+        app.logger.error("Failed to fetch indoor temperature: %s", e)
+        inside_temperature = None
+
+    try:
+        weather = get_weather()
+    except Exception as e:
+        app.logger.error("Failed to fetch weather: %s", e)
+        weather = None
+
+    try:
+        cal_events = fetch_calendar()
+    except Exception as e:
+        app.logger.error("Failed to fetch calendar events: %s", e)
+        cal_events = None
+
+    prompt = f"Write me a good {get_time_of_day()} message without using emojis or signing the message, given the following weather conditions for today:\n"
+
+    if inside_temperature:
+        prompt += f"Temperature inside the house: {inside_temperature}°F\n"
+
+    if weather:
+        prompt += f"{weather}\n"
+
     if cal_events:
-        prompt += f"\n\nAnd Today's calendar events: {', '.join(cal_events)}"
+        prompt += f"And Today's calendar events: {cal_events}\n"
+
+    if good_news:
+        prompt += f"Finally, here's a link to some good news that I'd like you to summarize: {good_news}\n"
     app.logger.info(prompt)
     return prompt
 
 
 def build_response() -> str:
-    prompt = build_prompt(*get_ha_data())
+    prompt = build_prompt()
 
     response = client.chat.completions.create(
         messages=[
@@ -127,15 +187,6 @@ def build_response() -> str:
     )
 
     return response.choices[0].message.content
-
-
-def _write_message() -> str:
-    message = build_response()
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    with open(f"{dir_path}/message.txt", "w") as f:
-        f.write(message)
-
-    return message
 
 
 @app.route("/")
